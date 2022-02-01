fa78e2b79246c2da8461e50304b45cd9c0a4201d
BonjourInternet <bonjour@bonjourinternet.top>
2022-02-01 15:46:59 +0100

    [feat] presets

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import './style.css'
 import { characterDistributions, getTiles, hueRotations, jitter } from "./helpers"
 
 const options = {
+  backgroundColor: '#000',
   luminanceFactor: 100,
-  invert: false,
   hueMin: 0,
   hueMax: 10,
   saturation: 100,
@@ -25,11 +25,63 @@ const img =  document.querySelector('img')
 const canvas = document.querySelector('#asciified')
 const ctx = canvas.getContext('2d')
 
+const createModal = (content, readonly=false) => {
+  const overlay = document.createElement('div')
+  overlay.className = 'modal-overlay'
+
+  const modal = document.createElement('modal')
+
+  const escText = document.createElement('p')
+  escText.innerText = '(press escape to close)'
+  modal.appendChild(escText)
+
+  const textarea = document.createElement('textarea')
+  textarea.innerText = content
+  textarea.readOnly = readonly
+  textarea.addEventListener('click', e => e.target.select())
+  modal.appendChild(textarea)
+
+  if(!readonly){
+    const loadPreset = document.createElement('button')
+    loadPreset.className = "load-button"
+    loadPreset.innerText = "Load preset"
+    loadPreset.addEventListener('click', () => {
+      const values = JSON.parse(textarea.value)
+      Object.entries(values).forEach(([k, v]) => {
+        if(options.hasOwnProperty(k)){
+          options[k] = v
+          document.getElementById(k).value = v
+        }
+      })
+
+      render()
+      overlay.remove()
+    })
+
+    modal.appendChild(loadPreset)
+  }
+  
+  overlay.appendChild(modal)
+
+  document.addEventListener('keyup', e => {
+    if(e.code === 'Escape'){
+      overlay.remove()
+    }
+  })
+
+  overlay.addEventListener('click', e => {
+    if(e.target.className === 'modal-overlay')
+      overlay.remove()
+  })
+
+  document.body.appendChild(overlay)
+}
+
 const render = () => {
   canvas.height = img.height
   canvas.width = img.width
 
-  ctx.fillStyle = options.invert ? 'white' : 'black'
+  ctx.fillStyle = options.backgroundColor
   ctx.fillRect(0, 0, img.width, img.height)
   ctx.textBaseline = 'top'
   ctx.textAlign = 'left'
@@ -42,8 +94,6 @@ const render = () => {
   getTiles(img, (id, i, j) => {
     const [r, g, b] = id
     let luminance = (r + g + b) / (255 * 3) * 100 * (options.luminanceFactor / 100) | 0
-    if(options.invert)
-      luminance = 100 - luminance
     
     const hueR = hueRotations[options.hueRotation](minHue, maxHue, hue, direction)
     hue = hueR[0]
@@ -67,7 +117,7 @@ for (const hueOptions in hueRotations) {
   opt.value = hueOptions
   hueRotationSelect.appendChild(opt)
 }
-const characterDistributionSelect = document.querySelector('#charDistribution')
+const characterDistributionSelect = document.querySelector('#characterDistribution')
 for (const cdOptions in characterDistributions) {
   const opt = document.createElement('option')
   opt.innerText = cdOptions
@@ -77,7 +127,7 @@ for (const cdOptions in characterDistributions) {
 
 document.querySelectorAll('.param_input').forEach(el => {
   el.addEventListener('change', ({target}) => {
-    options[target.name] = target.type === 'checkbox' ? target.checked : target.value
+    options[target.id] = target.type === 'checkbox' ? target.checked : target.value
     render()
   })
 })
@@ -88,6 +138,14 @@ document.querySelector('#change_file').addEventListener('change', e => {
     img.src = event.target.result
   })
   reader.readAsDataURL(e.target.files[0])
+})
+
+document.querySelector('#export-preset').addEventListener('click', () => {
+  createModal(JSON.stringify(options), true)
+})
+
+document.querySelector('#import-preset').addEventListener('click', () => {
+  createModal('', false)
 })
 
 render()
