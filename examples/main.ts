b5e8722212817e2d02d16729238787997cb821c2
BonjourInternet <bonjour@bonjourinternet.top>
2022-05-31 19:35:49 +0200

    chore: vite config (netlify deploy)

--- examples/main.ts
+++ examples/main.ts
@@ -3,22 +3,24 @@ import { CharacterDistributions, HueRotations } from "../src/types"
 
 const enluminure = new Enluminure()
 
-const populateSelect = (select, _enum) => {
-  Object.values(_enum).forEach(v => {
+const populateSelect = (select: HTMLSelectElement, _enum: any) => {
+  Object.values(_enum).forEach((v: string) => {
     const option = document.createElement('option')
     option.value = v
     option.innerText = v
     select?.appendChild(option)
   })
 }
-
+// @ts-ignore
 populateSelect(document.getElementById('hueRotation'), HueRotations)
+// @ts-ignore
 populateSelect(document.getElementById('characterDistribution'), CharacterDistributions)
 
 
 Object.entries(enluminure.getOptions()).forEach(([optionName, optionValue]) => {
   const input = document.getElementById(optionName)
   if(input){
+    // @ts-ignore
     input.value = optionValue
   }
 })
@@ -40,6 +42,7 @@ document
 .querySelectorAll('.param_input')
 .forEach(item => (
   item.addEventListener('change', async ({target}) => {
+    // @ts-ignore
     const { value, id } = target
     enluminure.setOptions({
       [id]: value
@@ -51,6 +54,7 @@ document
 
 document.querySelector('#change_file')
 ?.addEventListener('change', async ({target}) => {
+  // @ts-ignore
   await enluminure.loadImage(target.files[0])
   renderAndDisplay()
 })
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
 export default {
   root: 'examples',
+  build: {
+    outDir: '../dist',
+  },
 }
